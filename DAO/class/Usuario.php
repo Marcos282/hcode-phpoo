d20cff264e85ae9deed03ac67cd8f7353eef9065
Marcos Sousa <novahost@gmail.com>
2018-10-02 14:57:58 -0300

    Usuario ok

--- DAO/class/Usuario.php
+++ DAO/class/Usuario.php
@@ -1,13 +1,70 @@
-<!DOCTYPE html>
+<?php
 
-<html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
+class Usuario{
+    private $idusuario;
+    private $deslogin;
+    private $dessenha;
+    private $dtcadastro;
+           
+    function getIdusuario() {
+        return $this->idusuario;
+    }
+
+    function getDeslogin() {
+        return $this->deslogin;
+    }
+
+    function getDessenha() {
+        return $this->dessenha;
+    }
+
+    function getDtcadastro() {
+        return $this->dtcadastro;
+    }
+
+    function setIdusuario($idusuario) {
+        $this->idusuario = $idusuario;
+    }
+
+    function setDeslogin($deslogin) {
+        $this->deslogin = $deslogin;
+    }
+
+    function setDessenha($dessenha) {
+        $this->dessenha = $dessenha;
+    }
+
+    function setDtcadastro($dtcadastro) {
+        $this->dtcadastro = $dtcadastro;
+    }
+
+    public function loadById($id) {
+        $sql = new Sql();
         
-        ?>
-    </body>
-</html>
+        $result = $sql->select("SELECT * from tb_usuario WHERE id = :ID",array(
+            ":ID"=>$id
+        ));
+                
+        if(count($result[0])> 0){
+            $row = $result[0];
+            
+            $this->setIdusuario($row['id']);
+            $this->setDeslogin($row['login']);
+            $this->setDessenha($row['senha']);
+            $this->setDtcadastro($row['dtcadastro']);
+        }
+    }
+    
+    public function __toString() {
+        return json_encode(array(
+            "id"=>  $this->getIdusuario(),
+            "login"=>  $this->getDeslogin(),
+            "senha"=>  $this->getDessenha(),
+            "dtcadastro"=>  $this->getDtcadastro(),
+        ));
+    }
+       
+}
+
+
+?>
--- DAO/index.php
+++ DAO/index.php
@@ -1,24 +1,35 @@
 <?php
 
 
-
 require_once "config.php";
 
+$marcos = new Usuario;
 
-$sql = new Sql();
-
-$result = $sql->select("SELECT * from cadastro");
-
-//echo json_encode($result); 
-foreach ($result as $key => $value) {
-      
-    foreach ($value as $key => $valor) {
-        echo $key . ": ";
+$marcos->loadById(1);
         
-        echo $valor;
-        echo "<br>";
-    }
+echo $marcos;
+        
+//
+//$sql = new Sql();
+//
+//$result = $sql->select("SELECT * from cadastro");
+//
+////echo json_encode($result); 
+//foreach ($result as $key => $value) {
+//      
+//    foreach ($value as $key => $valor) {
+//        echo $key . ": ";
+//        
+//        echo $valor;
+//        echo "<br>";
+//    }
+//    
+//echo "==========================<br>";
+//
+//
+
+
+
+
     
-echo "==========================<br>";
-    
-}
+//}
